e7fc5ab3f130581c45c82ed5d5a48caeed0fe26f
Paulo Pacitti <ppacitti@outlook.com>
2019-11-03 22:35:42 -0300

    Adiciona algumas funcoes a Biblioteca de Controle.

--- bico.s
+++ bico.s
@@ -1,1 +1,46 @@
-// Implementação da camada BiCo
+# functions descriptions are in api_robot.h
+
+# args -> a0: Endereço do registro (com três valores inteiros) para armazenar as coordenadas (x, y, z);
+# return -> void (the return is in the a0)
+.globl get_current_GPS_position
+get_current_GPS_position:
+  li a7, 19
+  ecall
+
+  ret
+
+# args -> a0: Endereço do registro (com três valores inteiros) para armazenar os ângulos de Euler (x, y, z);
+# return -> void (the return is in the a0)	
+.globl get_gyro_angles
+get_gyro_angles:
+  li a7, 20
+  ecall
+
+  ret
+
+# args -> none
+# return -> a0: tempo do sistema em milisegundos;
+.globl get_time
+get_time:
+  li a7, 21
+  ecall
+
+  ret
+
+# args -> a0: tempo do sistema, em milisegundos;
+# return -> void;
+.globl set_time
+set_time:
+  li a7, 22
+  ecall
+
+  ret
+
+# args -> a0: Descritor do arquivo, a1: Endereço de memória do buffer a ser escrito, a2: Número de bytes a serem escritos;
+# return -> a0: Número de bytes efetivamente escritos;
+.globl puts
+puts:
+  li a7, 64
+  ecall
+
+  ret
