0267c4cdc8c4e560fa31f6007a5c6709e9160031
caiopardal <pardalcaio@hotmail.com>
2019-11-09 11:16:27 -0300

    Started loco implementation with two auxiliar functions

--- loco.c
+++ loco.c
@@ -1,1 +1,17 @@
-//Implementação da camada LoCo do uóli
+#include "api_robot2.h"
+
+void turnBaseDirection (int direction) {
+  if (direction == 1) { // direction = counter-clockwise
+    set_engine_torque(1, 30); // turns the direction for walle's body
+  } else if (direction == 0) { // direction = clockwise
+    set_engine_torque(0, 30); // turns the direction for walle's body
+  }
+}
+
+int detectObstacles () {
+  if (get_us_distance() != -1) { // if some distance is found, then there are obstacles
+    return 1;
+  }
+
+  return 0; // no obstacles
+}
